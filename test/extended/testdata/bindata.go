0774bf5524a75682054fbc355da2582797cd836d
Miciah Masters <miciah.masters@gmail.com>
2019-08-15 16:43:49 -0400

    test/extended/router: Enable metrics, fix readiness
    Fix the router template test fixtures that use the router's /healthz/ready
    endpoint for their readiness probes to enable HAProxy metrics, which are
    required for the endpoint to return OK.
    
    Without HAProxy metrics enabled, the router pods that tests created using
    these fixtures never became ready.
    
    Follow-up to commit b343fb79db16e317fade6dbdc2b5bf29451c7f26.
    
    * test/extended/testdata/router/router-override-domains.yaml:
    * test/extended/testdata/router/router-override.yaml:
    * test/extended/testdata/router/router-scoped.yaml: Enable HAProxy metrics.
    * test/extended/testdata/bindata.go: Regenerate.

--- test/extended/testdata/bindata.go
+++ test/extended/testdata/bindata.go
@@ -54014,6 +54014,8 @@ objects:
       - "--loglevel=4"
       - "--override-domains=null.ptr,void.str"
       - "--hostname-template=${name}-${namespace}.apps.veto.test"
+      - "--stats-port=1936"
+      - "--metrics-type=haproxy"
       hostNetwork: false
       ports:
       - containerPort: 80
@@ -54075,6 +54077,8 @@ objects:
       - "--loglevel=4"
       - "--override-hostname"
       - "--hostname-template=${name}-${namespace}.myapps.mycompany.com"
+      - "--stats-port=1936"
+      - "--metrics-type=haproxy"
       hostNetwork: false
       ports:
       - containerPort: 80
@@ -54139,6 +54143,8 @@ objects:
       - "--update-status=${UPDATE_STATUS}"
       - "--loglevel=4"
       - "--labels=select=first"
+      - "--stats-port=1936"
+      - "--metrics-type=haproxy"
       hostNetwork: false
       ports:
       - containerPort: 80
