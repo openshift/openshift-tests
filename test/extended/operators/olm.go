716b9c61b32cc4f9b9bfba1918ff7f02a708300c
Tom Buskey <tbuskey@redhat.com>
2020-04-15 13:03:06 -0400

    OCP-23670 reconcile with master

--- test/extended/operators/olm.go
+++ test/extended/operators/olm.go
@@ -160,8 +160,8 @@ var _ = g.Describe("[Feature:Platform] OLM should", func() {
 	})
 
 	// author: tbuskey@redhat.com
-	g.It("OLM-Low-OCP-23670-Checking description info for csc CRD￼", func() {
-		olmExplain := "csc"
+	g.It("OLM-Low-OCP-23670-Checking description info for csc CR", func() {
+		olmExplain := "CatalogSourceConfig"
 		msg, err := oc.AsAdmin().WithoutNamespace().Run("explain").Args(olmExplain).Output()
 		if err != nil {
 			e2e.Failf("Could not run oc explain %v:\n%v", olmExplain, msg)
