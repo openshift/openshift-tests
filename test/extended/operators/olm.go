8550cb5cabd795d6ed3df0c94a97d814574b0cfa
Kui Wang <kuiwang@redhat.com>
2020-12-30 14:20:59 +0800

    fix 25855

--- test/extended/operators/olm.go
+++ test/extended/operators/olm.go
@@ -2,6 +2,7 @@ package operators
 
 import (
 	"fmt"
+	"os/exec"
 	"regexp"
 
 	g "github.com/onsi/ginkgo"
@@ -172,7 +173,6 @@ var _ = g.Describe("[sig-operators] OLM for an end user use", func() {
 		defer sub.getCSV().delete(itName, dr)
 		newCheck("expect", asAdmin, true, compare, "Succeeded", ok, []string{"csv", sub.installedCSV, "-o=jsonpath={.status.phase}"}).check(oc)
 
-
 		g.By("Switch to common user to create the resources provided by the operator")
 		etcdClusterName := "example-etcd-cluster"
 		configFile, err := oc.Run("process").Args("-f", etcdCluster, "-p", fmt.Sprintf("NAME=%s", etcdClusterName), fmt.Sprintf("NAMESPACE=%s", oc.Namespace())).OutputToFile("config.json")
@@ -276,9 +276,8 @@ var _ = g.Describe("[sig-operators] OLM for an end user handle within a namespac
 
 		g.By("check the subscription_sync_total")
 		err := wait.Poll(10*time.Second, 120*time.Second, func() (bool, error) {
-			subscriptionSyncTotal := execResource(oc, asAdmin, withoutNamespace, "-c", "catalog-operator", infoCatalogOperator[0], "-n", "openshift-operator-lifecycle-manager", "--", "curl",
-				"-s", "-k", "-H \"Authorization: Bearer $(oc sa get-token prometheus-k8s -n openshift-monitoring)\"", fmt.Sprintf("https://%s/metrics", infoCatalogOperator[1]))
-			if !strings.Contains(subscriptionSyncTotal, sub.installedCSV) {
+			subscriptionSyncTotal, _ := exec.Command("bash", "-c", "oc exec -c catalog-operator "+infoCatalogOperator[0]+" -n openshift-operator-lifecycle-manager -- curl -s -k -H 'Authorization: Bearer $(oc sa get-token prometheus-k8s -n openshift-monitoring)' https://"+infoCatalogOperator[1]+"/metrics").Output()
+			if !strings.Contains(string(subscriptionSyncTotal), sub.installedCSV) {
 				e2e.Logf("the metric is not counted and try next round")
 				return false, nil
 			}
